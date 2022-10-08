849944e720dcad871bc47d96675e8e44c9b5147e
aluno <aluno@udicentro.local>
2022-10-07 21:42:38 -0300

    frist commit

---
+++ Sercretaria/Sercretaria/src/main/java/com/clinica/Sercretaria/SercretariaApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.Sercretaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercretariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SercretariaApplication.class, args);
+	}
+
+}
