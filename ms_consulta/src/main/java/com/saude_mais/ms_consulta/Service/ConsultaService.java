eacfffe08f0c8eeac30a00cf17bb5f84695d13a8
aluno <aluno@udicentro.local>
2022-11-04 21:45:22 -0300

    att 2

--- ms_consulta/src/main/java/com/saude_mais/ms_consulta/Service/ConsultaService.java
+++ ms_consulta/src/main/java/com/saude_mais/ms_consulta/Service/ConsultaService.java
@@ -1,13 +1,13 @@
 package com.saude_mais.ms_consulta.Service;
 
-import com.netflix.discovery.converters.Auto;
+
 import com.saude_mais.ms_consulta.entities.Consulta;
 import com.saude_mais.ms_consulta.feigns.ConsultaFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class ConsultaService {
@@ -26,16 +26,6 @@ public class ConsultaService {
     public List<Consulta> findByPacienteName(String name){
         return consultaFeign.getByPacienteName(name);
     }
-
-    //futuras funcoes att consulta:  em caso de erro de digitação
-    //futuras funcoes deletar consulta: não sei ao certo se seria necessário
-    
-    //não seria necessário criar consulta ja que esta deriva d agendamento
-
-
-
-
-
 
     
 }
--- ms_consulta/src/main/java/com/saude_mais/ms_consulta/feigns/ConsultaFeign.java
+++ ms_consulta/src/main/java/com/saude_mais/ms_consulta/feigns/ConsultaFeign.java
@@ -2,7 +2,6 @@ package com.saude_mais.ms_consulta.feigns;
 
 import com.saude_mais.ms_consulta.entities.Consulta;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
